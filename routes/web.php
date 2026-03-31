71a3b82ef96b0d311787ba73467f474cb1023a6a
Ilhomjon-Xursandov <mrdeviceuzb@gmail.com>
2026-03-31 10:32:41 +0500

    front end fayillari yuklandi

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('salom', function (){
-    return "Salom laravel ishaldi";
+    return view('index');
 });
